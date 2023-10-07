194f00f8b72e48e98c27fdaef3a75df94f5779bc
Yubo Cai <yubo.cai@polytechnique.edu>
2023-10-08 01:46:27 +0200

    update with multistability_system 2

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = '0.1.1'
+VERSION = '0.1.5'
 DESCRIPTION = 'DQode is a Python Library to compute the optimal inner-quadratic quadratization and dissipative quadratization of a given polynomial ODE system.'
 
 setup(
